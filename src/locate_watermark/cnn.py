97be0fc1a3376ac13d42e5c4e217743fa2404ea8
geng-haoran <2000012975@stu.pku.edu.cn>
2023-06-01 00:06:38 +0800

    add a resnet18 to locate watermark

--- src/locate_watermark/cnn.py
+++ src/locate_watermark/cnn.py
@@ -3,109 +3,79 @@ import matplotlib.pyplot as plt
 import torchvision
 import torch.utils.data as Data
 import torch.nn as nn
-
+from torchvision.models import resnet18
+from torchvision import transforms
 #设置随机种子以便复现
 torch.manual_seed(1)
 EPOCH=1
-BATCH_SIZE=64
+BATCH_SIZE=4
 LR=0.001
+from PIL import Image
+import torch
+ 
+class MyDataset(torch.utils.data.Dataset): #创建自己的类：MyDataset,这个类是继承的torch.utils.data.Dataset
+    def __init__(self, datatxt, transform=None, target_transform=None): #初始化一些需要传入的参数
+        fh = open(datatxt, 'r') #按照传入的路径和txt文本参数，打开这个文本，并读取内容
+        imgs = []                      #创建一个名为img的空列表，一会儿用来装东西
+        for line in fh:                #按行循环txt文本中的内容
+            line = line.rstrip()       # 删除 本行string 字符串末尾的指定字符，这个方法的详细介绍自己查询python
+            words = line.split()   #通过指定分隔符对字符串进行切片，默认为所有的空字符，包括空格、换行、制表符等
+            imgs.append([words[0],[float(words[1]), float(words[2]), float(words[3]), float(words[4])]]) #把txt里的内容读入imgs列表保存，具体是words几要看txt内容而定
+                                        # 很显然，根据我刚才截图所示txt的内容，words[0]是图片信息，words[1]是lable
+        self.imgs = imgs
+        self.transform = transform
+        self.target_transform = target_transform
+ 
+    def __getitem__(self, index):    #这个方法是必须要有的，用于按照索引读取每个元素的具体内容
+        fn, label = self.imgs[index] #fn是图片path #fn和label分别获得imgs[index]也即是刚才每行中word[0]和word[1]的信息
+        img = Image.open(fn).convert('RGB') #按照path读入图片from PIL import Image # 按照路径读取图片
+ 
+        if self.transform is not None:
+            img = self.transform(img) #是否进行transform
+        label = torch.FloatTensor(label)
+        return img,label  #return很关键，return回哪些内容，那么我们在训练时循环读取每个batch时，就能获得哪些内容
+ 
+    def __len__(self): #这个函数也必须要写，它返回的是数据集的长度，也就是多少张图片，要和loader的长度作区分
+        return len(self.imgs)
+ 
+#根据自己定义的那个勒MyDataset来创建数据集！注意是数据集！而不是loader迭代器
+train_data_path = 'data/training_data/cropped/data.txt'
+train_data=MyDataset(datatxt=train_data_path, transform=transforms.ToTensor())
+# test_data=MyDataset(txt=root+'test.txt', transform=transforms.ToTensor())
+train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
 
-#如果已经下载好改为False
-DOWNLOAD_MINIST= False
-#获取手写数字训练集
-train_data = torchvision.datasets.MNIST(
-    # 保存地点
-    root="./minist/",
-    # 是否是训练集
-    train=True,
-    # 转换 PIL.Image or numpy.ndarray 成torch.FloatTensor (C,H,W), 训练的时候 normalize 成 [0.0, 1.0]区间
-    transform=torchvision.transforms.ToTensor(),
-    # 是否下载
-    download=DOWNLOAD_MINIST)
-# 可视化查看一下数据图片
 
-train,label = train_data[0]
-plt.imshow(train.squeeze(), cmap='gray')
-plt.savefig("test.jpg")
-test_data = torchvision.datasets.MNIST(
-    # 保存地点
-    root="./minist/",
-    # 是否是训练集
-    train=False
-)
 
-# 批训练(32,1,28,28)
-train_loader = Data.DataLoader(dataset=train_data,batch_size=BATCH_SIZE,shuffle=True)
-# 由原来的(60000,28,28)变为(60000,1,28,28)
-test_x = torch.unsqueeze(test_data.data, dim=1).type(torch.FloatTensor)[:2000]/255.
-test_y = test_data.targets[:2000]
-print(test_x.size())
-print(test_y.size())
-
+Net = resnet18(num_classes=4)
 # 若存在cuda环境，即可使用注释代码
-test_x = test_x.cuda()
-test_y = test_y.cuda()
-
-
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN,self).__init__()
-        #输入形状为(1,128,128)，输出形状为(16,128,128)
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=3,   # 输入通道
-                      out_channels=16, # 输出通道
-                      kernel_size=5,   # 卷积核大小
-                      stride=1,        # 步幅
-                      padding=2),      # 填充
-            # 此时输出形状为(16,128,128)
-            nn.ReLU(),                 # 激活函数
-            # 最大池化，核大小为2，此时输出形状(16,64,64)
-            nn.MaxPool2d(kernel_size=2)
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(16,32,5,1,2), # 输出形状(32,64,64)
-            nn.ReLU(),
-            nn.MaxPool2d(2)         #输出形状(32,32,32)
-        )
-        # Fully connected, output bounding box's coordinates
-        self.out = nn.Linear(32*32*32, 4)
-
-    def forward(self,x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        # 将卷积层展平，才能输入全连接网络
-        x = x.view(x.size(0),-1)
-        output = self.out(x)
-        return output
-
-cnn = CNN()
-# 若存在cuda环境，即可使用注释代码
-cnn = cnn.cuda()
-
-# Optimizer
-optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
-
-# Loss function
+Net = Net.cuda()
+# print(Net)
+# 优化器
+optimizer = torch.optim.Adam(Net.parameters(),lr=LR)
+# 损失函数，分类问题
 loss_fn = nn.MSELoss()
 
 for epoch in range(EPOCH):
     # 迭代运行
-    for step, (img, bbox) in enumerate(train_loader):
-        img = img.cuda()
-        bbox = bbox.cuda()
-        output = cnn(img)
-        loss = loss_fn(output, bbox)
+    for step, (x, y) in enumerate(train_loader):
+        # print(x.size())
+        # print(y.size())
+        x = x.cuda()
+        y = y.cuda()
+        output = Net(x)
+        # print(output)
+        loss = loss_fn(output, y)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        if(step % 50 == 0):
+        if(step % 10 == 0):
             print('Epoch: ',epoch, '| train loss: %.4f' % loss.data)
-
-# 最后可以模型保存
-test_output = cnn(test_x[:10])
-print(test_output)
-print(torch.max(test_output, 1))
-print(torch.max(test_output, 1)[1])
-pred_y = torch.max(test_output, 1)[1]
-print(pred_y, 'prediction number')
-print(test_y[:10].squeeze(), 'real number')
+            # # 可以单独进行模型的测试
+            # test_output = Net(test_x)
+            # # 1代表索引列，因为刚好匹配到0-9，获取概率高的
+            # # pre_y = torch.max(test_output, 1)[1].data.squeeze()
+            
+            # pre_y = torch.max(test_output, 1)[1]
+            # # 获取准确率
+            # accurary = float((pre_y == test_y).sum()) / float(test_y.size(0))
+            # print('Epoch: ',epoch, '| train loss: %.4f' % loss.data, '| test accurary: %.2f' % accurary)
