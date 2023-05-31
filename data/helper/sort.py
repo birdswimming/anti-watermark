24c3b5163eed24fc4005ca42bb1f2aec817fae9f
Bingxuan_Li@PKU <lbx@stu.pku.edu.cn>
2023-05-31 21:27:02 +0800

    Update data

--- data/helper/sort.py
+++
@@ -1,9 +0,0 @@
-from glob import glob
-import os
-path = os.path.join('./wild/*.jpg')
-files = glob(path)
-for i,f in enumerate(files):
-    a = f.split('/')
-    b = a[-1].split('.')
-    all_path = os.path.join('./wild/', str(i+1) + '.' + b[-1])
-    os.rename(f, all_path)
--- src/add_watermark/test.py
+++ src/add_watermark/test.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+from random import random
 
 def Add_Watermark(src_path, out_path, font_size_ratio = 0.04, distance_ratio = 0.05, random = 0):
     im = Image.open(src_path)
@@ -17,7 +18,6 @@ def Add_Watermark(src_path, out_path, font_size_ratio = 0.04, distance_ratio = 0
     draw.bitmap((distance, im.height - 1.90*distance), bitmap, '#4A484C')
     im.save(out_path)
 
-
-src_path = 'data/training_data/origin/001.jpg'
-out_path = 'data/training_data/with_watermark/001.jpg'
+src_path = 'data/source_data/birds/00001.jpg'
+out_path = 'data/source_data/birds_wm/00001_wm.jpg'
 Add_Watermark(src_path, out_path)
